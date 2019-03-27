73d4f21727df54768be50314322396a25ecf18dd
Gustavo Giménez <ggimenez@modyo.com>
2019-03-27 11:46:52 -0300

    add count down timer

--- lib/papelito_web/live/game/bowl.ex
+++ lib/papelito_web/live/game/bowl.ex
@@ -13,9 +13,47 @@ defmodule PapelitoWeb.GameLive.Bowl do
       true ->
         summary = Papelito.GameManager.summary(game_id)
         # Papelito.LockManager.lock_bowl(game_id)
-        {:ok, assign(socket, %{game_name: game_id, summary: summary})}
+        {:ok, assign(socket, %{game_name: game_id, summary: summary, timer: 10, playing: false})}
     end
   end
 
   def render(assigns), do: PapelitoWeb.GameView.render("bowl.html", assigns)
+
+  def handle_event("pass_paper", _value, socket) do
+    # {:noreply, assign(socket, %{})}
+    {:noreply, socket}
+  end
+
+  def handle_event("check_paper", _value, socket) do
+    # {:noreply, assign(socket, %{})}
+    {:noreply, socket}
+  end
+
+  def handle_event("start_timer", _value, socket) do
+    :timer.send_after(1000, self(), :start_countdown)
+    {:noreply, assign(socket, %{playing: true, timer: socket.assigns.timer})}
+  end
+
+  def handle_event("stop_timer", _value, socket) do
+  end
+
+  def handle_info(:stop_timer, socket) do
+  end
+
+  def handle_info(:start_countdown, socket) do
+    timer = socket.assigns.timer
+
+    playing =
+      case socket.assigns.timer do
+        0 -> false
+        _ -> socket.assigns.playing
+      end
+
+    if socket.assigns.timer > 0 do
+      :timer.send_after(1000, self(), :start_countdown)
+      timer = timer - 1
+    end
+
+    {:noreply, assign(socket, %{timer: timer, playing: playing})}
+  end
 end
